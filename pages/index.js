c8a6ae7381cf67bfde3da15c7a3183f37ce2859e
Adam Fuhrer <adamfuhrer@hey.com>
2021-11-17 21:58:09 -0500

    Animate glitches

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,94 @@
 import Head from 'next/head'
 import {useEffect} from "react";
 
+var ctx;
+var canvas;
+var img;
+var glitchesArray = [];
+
 export default function Home() {
   useEffect(() => {
+    img = document.createElement("img");
+    img.src = 'https://images.genius.com/626ddf4c88de200d9487bb42449d1ae3.1000x1000x1.png';
+    canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx = canvas.getContext("2d");
+
+    setupGlitches();
     onGenerateClick()
   });
 
+  function Glitch(img, sourceX, sourceY, imgWidth, glitchHeight, destinationX) {
+    this.img = img;
+    this.sourceX = sourceX;
+    this.sourceY = sourceY;
+    this.imgWidth = imgWidth;
+    this.glitchHeight = glitchHeight
+    this.startX = randomNum(0, 1) ? 0 : imgWidth; // Animate glitch in from both left and right
+    this.destinationX = destinationX;
+
+    this.animate = () => {
+      ctx.drawImage(
+        this.img, 
+        this.sourceX,
+        this.sourceY,
+        this.imgWidth,
+        this.glitchHeight,
+        this.startX, // incrementor
+        this.sourceY,
+        this.imgWidth,
+        this.glitchHeight
+      )
+
+      if (this.startX < this.destinationX) {
+        this.startX++;
+      } else {
+        this.startX--;
+      }
+    }
+  }
+
+  function setupGlitches() {
+    let sourceY = 0;
+    const glitchHeight = 20;
+    const amountOfGlitches = window.innerHeight / glitchHeight;
+
+    for (let i = 0; i < amountOfGlitches; i++) {
+      let img = document.createElement("img");
+      img.src = 'https://images.genius.com/626ddf4c88de200d9487bb42449d1ae3.1000x1000x1.png';
+
+      let imgWidth = randomNum(100, window.innerWidth);
+      let destinationX = randomNum(0, 300);
+      let sourceX = randomNum(0, 300);
+
+      glitchesArray[i] = new Glitch(img, sourceX, sourceY, imgWidth, glitchHeight, destinationX) 
+      
+      sourceY = sourceY + glitchHeight;
+    } 
+  }
+
   function onGenerateClick() {
-    var img = document.createElement("img");
+    setupGlitches();
+    img = document.createElement("img");
     img.src = 'https://images.genius.com/626ddf4c88de200d9487bb42449d1ae3.1000x1000x1.png';
-    var canvas = document.getElementById("canvas");
+    canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    var ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, window.innerWidth, window.innerHeight);
+    // ctx.globalAlpha = randomNum(6, 10) * 0.1;
+    // ctx.globalCompositeOperation = "difference";
+    animateGlitches()
+  }
 
-    let glitchHeight = 10;
-    let glitchStepCounter = 0;
-    const amountOfGlitches = window.innerHeight / glitchHeight;
-    
-    img.addEventListener('load', function() {
-      ctx.drawImage(img, 0, 0, window.innerWidth, window.innerHeight);
-      
-      for (let i = 0; i < amountOfGlitches; i++) {
-        let img = document.createElement("img");
-        img.src = 'https://images.genius.com/626ddf4c88de200d9487bb42449d1ae3.1000x1000x1.png';
+  function animateGlitches() {
+    requestAnimationFrame(animateGlitches);
 
-        let imgWidth = randomNum(100, window.innerWidth);
-  
-        ctx.drawImage(
-          img, 
-          randomNum(0, 300), // source x
-          glitchStepCounter, // source y
-          imgWidth, // image width
-          glitchHeight, // image height
-          randomNum(0, 300), // dest x
-          glitchStepCounter, // dest y
-          imgWidth, // dest width
-          glitchHeight // dest height
-        )
-  
-        ctx.globalAlpha = randomNum(6, 10) * 0.1;
-        glitchStepCounter = glitchStepCounter + glitchHeight;
+    glitchesArray.forEach((glitch) => {
+      // One the startX and destinationX meet the glitch will stop animating
+      if (glitch.startX !== glitch.destinationX) {
+        glitch.animate();
       }
     });
   }
@@ -55,8 +103,7 @@ export default function Home() {
         <title>Glitch Image Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main >
+      <main>
         <canvas id="canvas"></canvas>
         <button className="generate-button" onClick={onGenerateClick}>
           GENERATE
